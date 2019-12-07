4de5a0446ab7054ba10121d3e5be2e452c266815
Andy Nguyen <apnguyen11@yahoo.com>
2019-12-06 18:17:59 -0600

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <h1 id="name">Tax Collection</h1>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
             
-        </div>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+            
+                <li className="nav-item">
+                    <a class="nav-link" href="index.html">About</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="./images.html">Images</a>
+                </li>
+                <li className="nav-item">
+                        <a className="nav-link" href="https://www.instagram.com/incometaxes/">Social</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
