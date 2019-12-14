d90600e086ca76550da40f731bad322c3d8bbaee
Andy Nguyen <apnguyen11@yahoo.com>
2019-12-14 13:14:28 -0600

    minor css change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import{ Link } from 'react-router-dom'
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <h1 id="name">Tax Collection</h1>
+            <h1 id="name">TAXPHOTO</h1>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
