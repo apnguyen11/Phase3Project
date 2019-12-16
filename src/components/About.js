ba124693d110fe750e2559de185968fd8130c1d4
Andy Nguyen <apnguyen11@yahoo.com>
2019-12-16 13:19:57 -0600

    bio info fix

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ function About() {
                     </div>
             
                 <p id="main"> 
-                        Born in Vietnam, photographer and skateboarder Phi Nguyen lives and works in Houston, TX. Initially stemming from his interest in videography, Phi started shooting photos soon after picking up a video camera after a few years. Through trial and error he has procured images of some of the most interesting and talented skateboarders in Houston. Not only this but his endeavors outside of his home town have also proven to be a testimonial to the incredible scene in his home town capturing the true talent of Texas natives. Furthermore, he has been fortunate enough to document up and coming pros and ams visitig from outside of Houston. Here is a short glance of his work...
+                        Born in Vietnam, photographer and skateboarder Phi Nguyen lives and works in Houston, TX. Initially stemming from his interest in videography, Phi started shooting photos soon after picking up a video camera after a few years. Through trial and error he has procured images of some of the most interesting and talented skateboarders in Houston. Not only this but his endeavors outside of his home town have also proven to be a testimonial to the incredible scene in Houston capturing the true talent of Texas natives. Furthermore, he has been fortunate enough to document up and coming pros and ams visiting from outside of the city. Here is a short glance of his work...
                 </p>
         </div>
     )
