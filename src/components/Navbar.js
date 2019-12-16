341860584743903813f7ebf5b1546d6b2ca48733
Andy Nguyen <apnguyen11@yahoo.com>
2019-12-15 18:02:35 -0600

    relinked main title to about page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import{ Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <h1 id="name">TAXPHOTO</h1>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{paddingTop:'2%'}}>
+            <h1 id="name" ><Link to="/about" style={{ textDecoration: 'none', color: 'black'}}>TAXPHOTO</Link></h1>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
